e4982c7613efe860607be6c84f74753a1822b9cb
jrc03c <jrc03c@pm.me>
2024-11-06 14:57:39 -0600

    ...

--- test/res/js/src/actions/enter-text.mjs
+++ test/res/js/src/actions/enter-text.mjs
@@ -1,3 +1,23 @@
-async function enterText() {}
+import { getTextInputElements } from "../utils/get-text-input-elements.mjs"
+
+async function enterText() {
+  const textString = text.toString()
+  const textInputElements = getTextInputElements()
+
+  if (textInputElements.length === 0) {
+    console.warn(
+      "No text input fields were found in which to put value:",
+      textString,
+    )
+
+    return
+  }
+
+  const element = textInputElements[0]
+  textInputElements.splice(0, 1)
+  element.value = textString
+  $(element).trigger("input")
+  console.log("entering text:", textString)
+}
 
 export { enterText }
